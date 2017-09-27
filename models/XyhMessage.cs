cd61e049db39b7d879b2de659aeeb1568ee3721b
姚苏国 <ysg@myschools.me>
2017-09-27 11:38:22 +0800

    修改枚举

--- models/XyhMessage.cs
+++ models/XyhMessage.cs
@@ -12,8 +12,8 @@ namespace com.nbugs.xyh.models
         public string orgId { get; set; }
         public string senderId { get; set; }
         public List<string> userIds { get; set; }
-        public List<string> deptIds { get; set; }
-        public List<string> userRoles { get; set; }
+        public string deptIds { get; set; }
+        public string userRoles { get; set; }
         public List<string> excludeUserIds { get; set; }
         public string withSubdepts { get; set; }
         public T msg { get; set; }
