4e7ab3cfc0f0913f731bad5260f4212e7fce9c82
姚苏国 <ysg@myschools.me>
2017-09-27 12:25:51 +0800

    大小写调整

--- models/XyhMessage.cs
+++ models/XyhMessage.cs
@@ -4,18 +4,18 @@ namespace com.nbugs.xyh.models
 {
     public class XyhMessage<T>
     {
-        public string msgId { get; set; }
-        public string funcId { get; set; }
+        public string msgid { get; set; }
+        public string funcid { get; set; }
         public string msgtype { get; set; }
         public string parentsReceive { get; set; }
         public string studentReceive { get; set; }
-        public string orgId { get; set; }
-        public string senderId { get; set; }
-        public string userIds { get; set; }
-        public string deptIds { get; set; }
-        public string userRoles { get; set; }
-        public string excludeUserIds { get; set; }
-        public string withSubdepts { get; set; }
+        public string orgid { get; set; }
+        public string senderid { get; set; }
+        public string userids { get; set; }
+        public string deptids { get; set; }
+        public string userroles { get; set; }
+        public string exclude_userids { get; set; }
+        public string with_subdepts { get; set; }
         public T msg { get; set; }
     }
 }
