bf89d0127c5c8faf6102ded5c588ee0063a67c0b
姚苏国 <ysg@myschools.me>
2017-09-27 11:54:26 +0800

    还要调整

--- models/XyhMessage.cs
+++ models/XyhMessage.cs
@@ -11,10 +11,10 @@ namespace com.nbugs.xyh.models
         public string studentReceive { get; set; }
         public string orgId { get; set; }
         public string senderId { get; set; }
-        public List<string> userIds { get; set; }
+        public string userIds { get; set; }
         public string deptIds { get; set; }
         public string userRoles { get; set; }
-        public List<string> excludeUserIds { get; set; }
+        public string excludeUserIds { get; set; }
         public string withSubdepts { get; set; }
         public T msg { get; set; }
     }
